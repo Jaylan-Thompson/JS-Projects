73820e10473149086b7a719157e229eb063f4063
Jaylan-Thompson <112976343+Jaylan-Thompson@users.noreply.github.com>
2022-09-28 13:13:28 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+//preforming a task
+function greet(name,lastName) {
+console.log('Hello '+ name + '' + lastName);
+}
+
+//calculating a value 
+function square(number){
+return number * number;
+}
+
+greet('Johnny ', 'Storm');
+
+//let number =square(2)
+console.log(square(2));
